4c1dba99e0deaa36c05cb0efb099b38eca422ed0
achref <achrefbenmbarek123@gmail.com>
2023-03-31 14:04:24 +0100

    walit kitebda connecte ou itji timchi lil login iredirectic lil articles

--- demo/components/AuthChecker.js
+++ demo/components/AuthChecker.js
@@ -3,18 +3,22 @@ import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 
 const AuthChecker = ({ children }) => {
-  const router = useRouter();
+    const router = useRouter();
 
-  if (typeof window !== 'undefined') {
-    // Only use the router if we're running in the browser
-    const authenticated = Cookies.get('authenticated');
-    if (!authenticated && router.pathname !== '/') {
-      router.push('/');
-      return null;
+    if (typeof window !== 'undefined') {
+        // Only use the router if we're running in the browser
+        const authenticated = Cookies.get('authenticated');
+        if (!authenticated && router.pathname !== '/') {
+            router.push('/');
+            return null;
+        }
+        else if (authenticated && router.pathname === '/') {
+            router.push('/articles');
+            return null;
+        }
     }
-  }
 
-  return children;
+    return children;
 };
 
 export default AuthChecker;
